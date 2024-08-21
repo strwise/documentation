b6297c39187f64ce896be2987aba54c250d6e6e2
Andre Kutianski <andre@eunarede.com>
2024-08-21 11:51:08 -0300

    feat: add sitemap xml

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,126 +7,132 @@ const darkCodeTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'StreamWise - VPaaS Resell Platform',
-  tagline: 'Welcome to StreamWise vPaaS, a Video Streaming Resell Platform. StreamWise is a powerful and customizable platform that enables businesses and individuals to offer high-quality video streaming services to their customers.',
-  favicon: 'img/favicon.png',
+    title: 'StreamWise - VPaaS Resell Platform',
+    tagline: 'Welcome to StreamWise vPaaS, a Video Streaming Resell Platform. StreamWise is a powerful and customizable platform that enables businesses and individuals to offer high-quality video streaming services to their customers.',
+    favicon: 'img/favicon.png',
 
-  // disable indexing of the site if DOMAIN is different from https://streamwise-vpaas-docs.pages.dev
+    // disable indexing of the site if DOMAIN is different from https://streamwise-vpaas-docs.pages.dev
     noIndex: process.env.DOMAIN !== 'https://streamwise-vpaas-docs.pages.dev',
 
-  // Set the production url of your site here
-  url: process.env.DOMAIN || 'https://streamwise-vpaas-docs.pages.dev',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+    // Set the production url of your site here
+    url: process.env.DOMAIN || 'https://streamwise-vpaas-docs.pages.dev',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'strwise', // Usually your GitHub org/user name.
-  projectName: 'documentation', // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'strwise', // Usually your GitHub org/user name.
+    projectName: 'documentation', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  trailingSlash: false,
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    trailingSlash: false,
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
 
-  markdown: {
-    mermaid: true,
-  },
-  themes: ['@docusaurus/theme-mermaid'],
+    markdown: {
+        mermaid: true,
+    },
+    themes: ['@docusaurus/theme-mermaid'],
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          showLastUpdateAuthor: false,
-          showLastUpdateTime: false,
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    showLastUpdateAuthor: false,
+                    showLastUpdateTime: false,
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+                sitemap: {
+                    lastmod: false,
+                    changefreq: 'weekly',
+                    priority: 1.0,
+                    filename: 'sitemap.xml',
+                },
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
+    themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      docs: {
-        sidebar: {
-          hideable: true,
-          autoCollapseCategories: true,
-        },
-      },
-      // Replace with your project's social card
-      image: 'img/streamwise-vpaas.jpg',
-      navbar: {
-        title: 'Home',
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Docs',
-          },
-          // {
-          //   href: 'https://api-reference.streamwise.online/private/',
-          //   position: 'right',
-          //   label: 'API Reference (private)',
-          // },
-          // {
-          //   href: 'https://api-reference.streamwise.online/public/',
-          //   position: 'right',
-          //   label: 'API Reference (public)',
-          // },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Overview',
-                to: '/docs/overview',
-              },
-            ],
-          },
-          {
-            title: 'API Reference',
-            items: [
-              {
-                label: 'API Reference (private)',
-                href: 'https://api-reference.streamwise.app/private',
-              },
-              {
-                label: 'API Reference (public)',
-                href: 'https://api-reference.streamwise.app/public',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Mimir Tech. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-        additionalLanguages: ['json', 'bash', 'diff', 'php', 'http'],
-      },
-      // add meta tag with Page Author
-      metadata: [{ name: 'author', content: 'Mimir Tech' }],
-    }),
+        ({
+            docs: {
+                sidebar: {
+                    hideable: true,
+                    autoCollapseCategories: true,
+                },
+            },
+            // Replace with your project's social card
+            image: 'img/streamwise-vpaas.jpg',
+            navbar: {
+                title: 'Home',
+                items: [
+                    {
+                        type: 'docSidebar',
+                        sidebarId: 'tutorialSidebar',
+                        position: 'left',
+                        label: 'Docs',
+                    },
+                    // {
+                    //   href: 'https://api-reference.streamwise.online/private/',
+                    //   position: 'right',
+                    //   label: 'API Reference (private)',
+                    // },
+                    // {
+                    //   href: 'https://api-reference.streamwise.online/public/',
+                    //   position: 'right',
+                    //   label: 'API Reference (public)',
+                    // },
+                ],
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'Docs',
+                        items: [
+                            {
+                                label: 'Overview',
+                                to: '/docs/overview',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'API Reference',
+                        items: [
+                            {
+                                label: 'API Reference (private)',
+                                href: 'https://api-reference.streamwise.app/private',
+                            },
+                            {
+                                label: 'API Reference (public)',
+                                href: 'https://api-reference.streamwise.app/public',
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} Mimir Tech. Built with Docusaurus.`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+                additionalLanguages: ['json', 'bash', 'diff', 'php', 'http'],
+            },
+            // add meta tag with Page Author
+            metadata: [{name: 'author', content: 'Mimir Tech'}],
+        }),
 };
 
 module.exports = config;
